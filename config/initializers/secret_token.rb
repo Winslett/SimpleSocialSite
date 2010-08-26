b8f77c74f2ad1e4b80c136cc38f0bb8ce1a3f83a
Christopher Winslett <chris@gradesfirst.com>
2010-08-25 22:20:44 -0500

    Beginning Rails site.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimpleSocialSite::Application.config.secret_token = 'ce8dfeff13845287ba4276b05fee7fbd2a99d3c443ab27c41dad14d5b52d8f2e79a07c8a914838b616c650c3e0ebb5e40d17b74d93b149ebaac159bb4f21210c'
